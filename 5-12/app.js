6c5b344efe0cf42ac78460f43146b0f521899c5a
gu12345611 <3625867530@qq.com>
2021-05-13 11:37:53 +0800

    vue js

---
+++ 5-12/app.js
@@ -0,0 +1,20 @@
+//引入 mysql 类库
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',         //数据库地址
+    user     : 'root',              //数据库用户名
+    password : '123456',            // 数据库密码
+    database : 'api2009'              // 数据库名
+});
+
+//建立连接
+connection.connect();
+
+//拼装sql语句
+let sql = "select * from p_users order by user_id desc limit 10"
+
+//执行查询
+connection.query(sql, function (error, results, fields) {
+    console.log(results)
+});
